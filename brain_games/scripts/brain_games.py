6c48a7e9711dfffe1017e1885d96a0ff0147f38d
Peter_007 <hihb3ya@gmail.com>
2024-07-30 19:37:04 +0300

    some fixes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-print('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
 
 
 def main():
-    None
+    print('Welcome to the Brain Games!')
+    welcome_user()
 
 
 if __name__ == '__main__':
