90a302f16d5ce858b147929eb2e3a6c6987da430
Peter_007 <hihb3ya@gmail.com>
2024-08-01 20:31:34 +0300

    added progression game

---
+++ brain_games/progression.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import prompt
+from random import randint
+
+
+def welcome_user_progression():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    print('What number is missing in the progression?')
+    return name
+
+
+def my_progression():
+    start = randint(1, 50)
+    step = randint(2, 10)
+    length = randint(5, 10)
+    progression = [start + i * step for i in range(length)]
+    return progression
+
+
+def replace_el():
+    progression = my_progression()
+    element = randint(0, len(progression) - 1)
+    old_el = progression[element]
+    progression[element] = '..'
+    return progression, old_el
+
+
+def progress_game():
+    name = welcome_user_progression()
+    correct_answers = 0
+
+    while correct_answers < 3:
+        progression, old_el = replace_el()
+        correct_answer = old_el
+        print('Question:', ' '.join(map(str, progression)))
+        user_answer = prompt.integer('Your answer: ')
+        if user_answer == correct_answer:
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(f'"{user_answer}" is wrong answer ;(. Correct answer '
+                  f'was "{correct_answer}".')
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from brain_games.progression import progress_game
+
+
+def main():
+    progress_game()
+
+
+if __name__ == '__main__':
+    main()
