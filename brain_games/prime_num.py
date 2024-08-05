16855f998bb06314aa2313980136f841250e5dc8
Peter_007 <hihb3ya@gmail.com>
2024-08-05 15:43:02 +0300

    update prime_num

--- brain_games/prime_num.py
+++ brain_games/prime_num.py
@@ -24,6 +24,12 @@ def prime_number(num):
         return True
     if num % 2 == 0 or num % 3 == 0:
         return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
 def game_prime_n():
